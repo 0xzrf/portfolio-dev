1941aee98284469736ac86f5615321bb436e4b2f
0xzrf <krishh16@yahoo.com>
2024-11-25 13:41:17 +0530

    rm

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Work  from "@/components/App/Work";
 import {Experience} from "@/components/App/Experience";
   
 
-
-
 export default function Home() {
   return (
    <div className="min-h-screen w-full  overflow-hidden" style={{ backgroundColor: '#18191a', paddingTop: "1px" }}>
